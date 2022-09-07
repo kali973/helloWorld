d27de06d5f5301c60b506045f5bfc1ae03438aac
coutard <gilles.coutard@telecom-paris.fr>
2022-09-07 15:00:43 -0400

    Initialisation TP01 Telecom Paris CYBER701 - Java

--- src/main/java/com/example/telecom/paris/Application.java
+++ src/main/java/com/example/telecom/paris/Application.java
@@ -1,15 +1,14 @@
 package com.example.telecom.paris;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @Slf4j
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		System.out.println("HelloWorld");
+		log.info("Application end");
 	}
-
 }
